dc0bb0858f0f166aff63b8ee57d7ea5badcafef9
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-08 15:11:08 +0300

    Add semicolons to the script and move the capitalize function from the global scope to the local scope of the playRound function

--- script.js
+++ script.js
@@ -1,77 +1,72 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*3)
     if (choice === 0) {
-        return "Rock"
+        return "Rock";
     }
     else if (choice === 1) {
-        return "Paper"
+        return "Paper";
     }
     else if (choice === 2) {
-        return "Scissors"
+        return "Scissors";
     }
 }
-
-function capitalize(s) {
-    s1 = s[0].toUpperCase()
-    s2 = s.slice(1).toLowerCase()
-    return s1+s2
-   }
 
 
 function playRound(playerSelection, computerSelection) {
 
     function capitalize(s) {
-        s1 = s[0].toUpperCase()
-        s2 = s.slice(1).toLowerCase()
-        return s1+s2
+        s1 = s[0].toUpperCase();
+        s2 = s.slice(1).toLowerCase();
+        return s1+s2;
        }
 
-    playerSelection = capitalize(playerSelection)
-    computerSelection = capitalize(computerSelection)
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
+
     if (playerSelection === "Rock") {
         if (computerSelection === "Rock") {
-            return "It's a Tie! Both chose Rock"
+            return "It's a Tie! Both chose Rock";
         }
         else if (computerSelection === "Paper") {
-            return "You Lose! Paper beats Rock"
+            return "You Lose! Paper beats Rock";
         }
         else if (computerSelection === "Scissors") {
-            return "You Win! Rock beats Scissors"
+            return "You Win! Rock beats Scissors";
         }
         else {
-            return "Invalid computerSelection input"
+            return "Invalid computerSelection input";
         }
     }
     else if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
-            return "You Win! Paper beats Rock"
+            return "You Win! Paper beats Rock";
         }
         else if (computerSelection === "Paper") {
-            return "It's a Tie! Both chose Paper"
+            return "It's a Tie! Both chose Paper";
         }
         else if (computerSelection === "Scissors") {
-            return "You Lose! Scissors beats Paper"
+            return "You Lose! Scissors beats Paper";
         }
         else {
-            return "Invalid computerSelection input"
+            return "Invalid computerSelection input";
         }
     }
     else if (playerSelection === "Scissors") {
         if (computerSelection === "Rock") {
-            return "You Lose! Rock beats Scissors"
+            return "You Lose! Rock beats Scissors";
         }
         else if (computerSelection === "Paper") {
-            return "You Win! Scissors beats Rock"
+            return "You Win! Scissors beats Rock";
         }
         else if (computerSelection === "Scissors") {
-            return "It's a Tie! Both chose Scissors"
+            return "It's a Tie! Both chose Scissors";
         }
         else {
-            return "Invalid computerSelection input"
+            return "Invalid computerSelection input";
         }
     }
     else {
-        return "Invalid playerSelection input"
+        return "Invalid playerSelection input";
     }
   }
    
