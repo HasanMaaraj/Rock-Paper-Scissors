5edbc084d94b373aff1980bd16543044a4207f5b
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-08 14:32:45 +0300

    Capialize the first letters from the returned values of getComputerChoice

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*3)
     if (choice === 0) {
-        return "rock"
+        return "Rock"
     }
     else if (choice === 1) {
-        return "paper"
+        return "Paper"
     }
     else if (choice === 2) {
-        return "scissors"
+        return "Scissors"
     }
 }
