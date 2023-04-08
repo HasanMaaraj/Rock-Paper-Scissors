259d860b81248e4b0cfbd9f84a5a87e930d07440
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-08 15:07:14 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -10,3 +10,68 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
+
+function capitalize(s) {
+    s1 = s[0].toUpperCase()
+    s2 = s.slice(1).toLowerCase()
+    return s1+s2
+   }
+
+
+function playRound(playerSelection, computerSelection) {
+
+    function capitalize(s) {
+        s1 = s[0].toUpperCase()
+        s2 = s.slice(1).toLowerCase()
+        return s1+s2
+       }
+
+    playerSelection = capitalize(playerSelection)
+    computerSelection = capitalize(computerSelection)
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+            return "It's a Tie! Both chose Rock"
+        }
+        else if (computerSelection === "Paper") {
+            return "You Lose! Paper beats Rock"
+        }
+        else if (computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors"
+        }
+        else {
+            return "Invalid computerSelection input"
+        }
+    }
+    else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return "You Win! Paper beats Rock"
+        }
+        else if (computerSelection === "Paper") {
+            return "It's a Tie! Both chose Paper"
+        }
+        else if (computerSelection === "Scissors") {
+            return "You Lose! Scissors beats Paper"
+        }
+        else {
+            return "Invalid computerSelection input"
+        }
+    }
+    else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            return "You Lose! Rock beats Scissors"
+        }
+        else if (computerSelection === "Paper") {
+            return "You Win! Scissors beats Rock"
+        }
+        else if (computerSelection === "Scissors") {
+            return "It's a Tie! Both chose Scissors"
+        }
+        else {
+            return "Invalid computerSelection input"
+        }
+    }
+    else {
+        return "Invalid playerSelection input"
+    }
+  }
+   
